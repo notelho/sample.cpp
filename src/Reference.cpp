178805cfd98559f4c5a657a7294e6682d96ecfc9
enbot <nimbotelho@gmail.com>
2020-01-26 18:30:20 -0300

    reference

---
+++ src/Reference.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    int x = 10;
+    int &y = x;
+
+    cout << x << endl;
+    cout << y << endl;
+
+    y++;
+
+    cout << x << endl;
+    cout << y << endl;
+
+    x++;
+
+    cout << x << endl;
+    cout << y << endl;
+
+    return 0;
+}
