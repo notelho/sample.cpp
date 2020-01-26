616e7e75b1b2a96fb6fb81f24e4707d069ce4f69
enbot <nimbotelho@gmail.com>
2020-01-26 20:29:46 -0300

    & ref vs =

--- src/Reference.cpp
+++ src/Reference.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
 
+    cout << "============================" << endl;
+
     int x = 10;
     int &y = x;
 
@@ -21,6 +23,26 @@ int main()
 
     cout << x << endl;
     cout << y << endl;
+
+    cout << "============================" << endl;
+
+    int w = 10;
+    int j = w;
+
+    cout << w << endl;
+    cout << j << endl;
+
+    j++;
+
+    cout << w << endl;
+    cout << j << endl;
+
+    w++;
+
+    cout << w << endl;
+    cout << j << endl;
+
+    cout << "============================" << endl;
 
     return 0;
 }
