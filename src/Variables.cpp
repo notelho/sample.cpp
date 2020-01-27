fcbe2413bdc812a3ecb051fc11fb6a6800694c67
enbot <nimbotelho@gmail.com>
2020-01-26 23:26:17 -0300

    testing variables call

--- src/Variables.cpp
+++ src/Variables.cpp
@@ -35,12 +35,12 @@ int main()
     // auto autotest = 1.1;
     // cout << "type: " << typeid(autotest).name() << endl;
 
-    cout << "types" << endl;
-    const int myNum = 5;
-    double myFloatNum = 5.99;
-    char myLetter = 'D';
-    string myText = "Hello";
-    bool myBoolean = true;
+    // cout << "types" << endl;
+    // const int myNum = 5;
+    // double myFloatNum = 5.99;
+    // char myLetter = 'D';
+    // string myText = "Hello";
+    // bool myBoolean = true;
 
     cout << "default sum" << endl;
     int x = 5, y = 6, z = 50;
@@ -58,6 +58,33 @@ int main()
     // Aggregate initialization, e.g. char a[3] = {'a', 'b'};
     // Reference initialization, e.g. char& c = a[0];
     // If no initializer is provided, the rules of default initialization apply.
+
+    cout << "bool tests" << endl;
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout << isCodingFun << endl; // Outputs 1 (true)
+    cout << isFishTasty << endl; // Outputs 0 (false)
+
+    cout << "scientific Numbers" << endl;
+    float ff1 = 35e3;
+    double dd1 = 12E4;
+    cout << ff1 << endl;
+    cout << dd1 << endl;
+
+    cout << "constructor tests" << endl;
+
+    // string s1{};
+    // cout << s1 << endl;
+    string s2("hello");
+    cout << s2 << endl;
+    string s3 = "hello";
+    cout << s3 << endl;
+    char s4[3]{'a', 'b', 'c'};
+    cout << s4 << endl;
+    char aaa[3] = {'a', 'b'};
+    cout << aaa << endl;
+    // char &c = a[0];
+    // cout << c << endl;
 
     return 0;
 }
