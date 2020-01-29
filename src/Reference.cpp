a43022ca0d67a96c4cae3c94b9ffaac1e7d6a397
enbot <nimbotelho@gmail.com>
2020-01-28 21:09:31 -0300

    testing pointers

--- src/Reference.cpp
+++ src/Reference.cpp
@@ -44,5 +44,40 @@ int main()
 
     cout << "============================" << endl;
 
+    string food = "Pizza"; // Variable declaration
+    string *ptr = &food;   // Pointer declaration
+    string &meal = food;
+
+    cout << food << "\n";
+    cout << meal << "\n";
+    cout << &food << "\n";
+
+    cout << "============================" << endl;
+
+    // Output the value of food (Pizza)
+    cout << food << "\n";
+    // Output the memory address of food (0x6dfed4)
+    cout << &food << "\n";
+    // Output the memory address of food with the pointer (0x6dfed4)
+    cout << ptr << "\n";
+
+    cout << "============================" << endl;
+
+    // Reference: Output the memory address of food with the pointer (0x6dfed4)
+    cout << ptr << "\n";
+    // Dereference: Output the value of food with the pointer (Pizza)
+    cout << *ptr << "\n";
+
+    cout << "============================" << endl;
+
+    // Change the value of the pointer
+    *ptr = "Hamburger";
+    // Output the new value of the pointer
+    cout << *ptr << "\n";
+    // Output the new value of the food variable
+    cout << food << "\n";
+
+    cout << "============================" << endl;
+
     return 0;
 }
